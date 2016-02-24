2152d1bcd01cbeed95849347dabc997cefaf770a
minno22 <minnockn@gmail.com>
2016-02-24 21:50:38 +0000

    Added teleport

--- MyRect.cpp
+++ MyRect.cpp
@@ -24,6 +24,31 @@ void MyRect::keyPressEvent(QKeyEvent *event){
         setPos(x(),y()+15);
         //qDebug() << "DOWN(s)";
         break;
+    case Qt::Key_Left:
+        setPos(x()-15,y());
+        //qDebug() << "LEFT(a)";
+        break;
+    case Qt::Key_Right:
+        setPos(x()+15,y());
+        //qDebug() << "RIGHT(d)";
+        break;
+    case Qt::Key_Up:
+        setPos(x(),y()-15);
+        //qDebug() << "UP(w)";
+        break;
+    case Qt::Key_Down:
+        setPos(x(),y()+15);
+        //qDebug() << "DOWN(s)";
+        break;
+    case Qt::Key_T:
+        switch (rand()%5){
+            case 0: setPos(300, 100);break;
+            case 1: setPos(300, 500);break;
+            case 2: setPos(600, 350);break;
+            case 3: setPos(900, 100);break;
+            case 4: setPos(900, 500);break;
+        //qDebug() << "TELEPORT(t)";
+        }
     default:
         //qDebug() << "Wrong KEY!!!";
         break;
