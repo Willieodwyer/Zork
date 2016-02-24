9cfc9babdf310dc48a6f26c72c8d6f02f6eed47b
Will <willieod666@live.com>
2016-02-24 20:47:25 +0000

    added resource

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 #include "MyRect.h"
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QtGlobal>
+
 
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
 
-    // create a scene
-    QGraphicsScene * scene = new QGraphicsScene();
-
     // create an item to add to the scene
     MyRect * player = new MyRect();
     player->setRect(0,0,100,100); // change the rect from 0x0 (default) to 100x100 pixels
 
+    QImage image("assets/img.png");
+
+    QGraphicsPixmapItem item( QPixmap::fromImage(image));
+    QGraphicsScene* scene = new QGraphicsScene;
+    scene->addItem(&item);
     // add the item to the scene
     scene->addItem(player);
 
