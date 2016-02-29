0c6b4a241e09b350d9476800dbf0735bf9532ba4
Will <willieod666@live.com>
2016-02-29 15:14:00 +0000

    Fixed all the shit

--- Player.cpp
+++ Player.cpp
@@ -4,12 +4,8 @@
 #include <QPixmap>
 #include <QGraphicsPixmapItem>
 #include <QPixmap>
-<<<<<<< HEAD
 #include <QFuture>
 #include <QtConcurrent/QtConcurrent>
-=======
-#include "Room.h"
->>>>>>> Wills
 #define SCALE 0.05
 #define DISTANCE 30
 
@@ -20,9 +16,6 @@
 
 
 Player::Player(){
-
-    hallway = new Room("hallway",120,510,180,660);
-
     setPixmap(QPixmap(":/assets/assets/pacman-ghost-128.png"));
     setScale(SCALE);
     setPos(x()+(DISTANCE*4),y()+(DISTANCE*21));
@@ -57,11 +50,9 @@ void Player::keyPressEvent(QKeyEvent *event){
             break;
         case Qt::Key_F2:
             qDebug() << "menu";
-            displayMenu();
             break;
         case Qt::Key_Space:
             qDebug() << "Scenario";
-            displayScenario();
             break;
         default:
             //qDebug() << "Wrong KEY!!!";
@@ -69,29 +60,9 @@ void Player::keyPressEvent(QKeyEvent *event){
         }
         qDebug() << "X: " +  QString::number(x()) + " Y: " + QString::number(y());
     }
-    else
-        qDebug() << "Not in a room innit";
-}
-
-void Player::displayMenu(){
-    menu.show();
-}
-
-void Player::displayScenario(){
-    sbox.setScenario("scenario title", "Insert scenario descrption here", "option 1", "option 2");
-    //QFuture<void> future = QtConcurrent::run(sbox.showScenario());// bool a = sbox.showScenario();
-    sbox.show();
-    qDebug() << sbox.optionSelected;
 }
 
 bool Player::isInRoom(){
-    if(hallway->getY1() >= (y() + DISTANCE)/* &&
-            y() >= (hallway->getY1() - DISTANCE) &&
-            x() <= (hallway->getX2() + DISTANCE) &&
-            y() <= (hallway->getY2() + DISTANCE)*/
-        )
-        return true;
-    else
-       return false;
+    return true;
 }
 
--- Player.h
+++ Player.h
@@ -2,29 +2,15 @@
 #define MYPLAYER_H
 
 #include <QGraphicsPixmapItem>
-<<<<<<< HEAD
 #include <QSet>
 #include "gamemenu.h"
 #include "scenariobox.h"
-
-class Player: public QGraphicsPixmapItem{
-
-private:
-    bool isInPlayableArea();
-    void displayMenu();
-    void displayScenario();
-    GameMenu menu;
-    ScenarioBox sbox;
-=======
 #include "Room.h"
 
 class Player: public QGraphicsPixmapItem{
 
->>>>>>> Wills
-
 public:
     Player();
-
     Room *hallway; // make this some sort of array of rooms
 
     void keyPressEvent(QKeyEvent * event);
