a5611871dcdf2cb5ea4860e461f34e0318996ff1
Will <willieod666@live.com>
2016-02-29 15:18:43 +0000

    Final Fixes

--- Player.cpp
+++ Player.cpp
@@ -25,34 +25,25 @@ void Player::keyPressEvent(QKeyEvent *event){
     if(isInRoom()){
         switch (event->key()) {
         //left righ up and down
-        case Qt::Key_A: case Qt::Key_Left:
+        case Qt::Key_A:
             setPixmap(QPixmap(":/assets/assets/pacman-ghost-128-flipped.png"));
             setPos(x()-DISTANCE,y());
             //qDebug() << "LEFT(a)";
             break;
-        case Qt::Key_D: case Qt::Key_Right:
+        case Qt::Key_D:
             setPixmap(QPixmap(":/assets/assets/pacman-ghost-128.png"));
             setPos(x()+DISTANCE,y());
             //qDebug() << "RIGHT(d)";
             break;
-        case Qt::Key_W: case Qt::Key_Up:
+        case Qt::Key_W:
             setPixmap(QPixmap(":/assets/assets/pacman-ghost-128-UP.png"));
             setPos(x(),y()-DISTANCE);
             //qDebug() << "UP(w)";
             break;
-        case Qt::Key_S: case Qt::Key_Down:
+        case Qt::Key_S:
             setPixmap(QPixmap(":/assets/assets/pacman-ghost-128-DOWN.png"));
             setPos(x(),y()+DISTANCE);
             //qDebug() << "DOWN(s)";
-            break;
-        case Qt::Key_F1:
-            qDebug() << "Help";
-            break;
-        case Qt::Key_F2:
-            qDebug() << "menu";
-            break;
-        case Qt::Key_Space:
-            qDebug() << "Scenario";
             break;
         default:
             //qDebug() << "Wrong KEY!!!";
@@ -65,4 +56,3 @@ void Player::keyPressEvent(QKeyEvent *event){
 bool Player::isInRoom(){
     return true;
 }
-
